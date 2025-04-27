a26fcf9e7e7121f9f1d90ed7db37264dd052f083
Student ApnaCollege <vivekanandkumawat261@gmail.com>
2025-04-27 18:06:48 +0530

    part - 1

---
+++ backend/src/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "dream_city_pro"
---
+++ backend/src/db/index.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
+
+
+
+/*
+import express from "express"
+const app = express()
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
